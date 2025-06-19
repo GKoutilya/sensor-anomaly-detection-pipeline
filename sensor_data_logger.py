cba45e3058fae78d7ef43e31f84a06213a237b6d
Koutilya Ganapathiraju <koutilyagraju@gmail.com>
2025-06-19 17:34:21 -0500

    Explored and Analyzed the dataset

--- sensor_data_logger.py
+++ sensor_data_logger.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("secom.csv")
 
-print(df.head())
+'''print(df.head())
 print(df.columns)
 print(df.info())
 print(df.describe())
+print(df.isna().sum())
+print(df.dtypes)
+print(df['0'].head(10))
+print(df['0'].tail(10))'''
+
+df_filled = df.fillna(df.mean())
+
+
+print(df_filled.head())
+print(df_filled.info())
+print(df_filled.describe())
